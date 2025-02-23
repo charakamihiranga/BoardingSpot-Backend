21a88be33c5b93b53bed3869f6aecd71ae6934fe
charaka mihiranga <charakavmihiranga@gmail.com>
2025-02-23 13:37:08 +0530

    add CRUD operations for FoodSupplier, including getAll, getById, update, and delete functionalities

--- src/controllers/foodSupplierController.ts
+++ src/controllers/foodSupplierController.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import {imageUploader} from "../utils/cloudinaryUtil";
+import {deleteImages, imageUploader} from "../utils/cloudinaryUtil";
 import {FoodSupplier} from "../models/FoodSupplier";
+import mongoose from "mongoose";
 
 export const addFoodSupplier = async (req: express.Request, res: express.Response) => {
     try {
-      const { fullName, location, mobileNo, description, foodType} = req.body;
+      const { fullName, location, city, mobileNo, description, foodType} = req.body;
       const price = req.body.price ? Number(req.body.price) : null;
       const userId = req.body.userId;
       const imageUrls = await imageUploader(req.files as Express.Multer.File[]);
@@ -12,6 +13,7 @@ export const addFoodSupplier = async (req: express.Request, res: express.Respons
       const foodSupplier = await FoodSupplier.create({
           fullName,
           location: typeof location === "string" ? JSON.parse(location) : location,
+          city,
           mobileNo,
           description,
           foodType,
@@ -24,6 +26,86 @@ export const addFoodSupplier = async (req: express.Request, res: express.Respons
       } else {
             res.status(400).json({message: 'Invalid food supplier data'});
       }
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({message: 'Internal server error'});
+    }
+}
+
+export const getAllFoodSuppliers = async (req: express.Request, res: express.Response) => {
+    try {
+        const foodSuppliers = await FoodSupplier.find();
+        res.status(200).json(foodSuppliers);
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({message: 'Internal server error'});
+    }
+}
+
+export const getFoodSupplierById = async (req: any, res: any) => {
+    try {
+        const { id } = req.params;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: "Invalid food supplier ID" });
+        }
+        const foodSupplier = await FoodSupplier.findById(id);
+        if (!foodSupplier) {
+            res.status(404).json({message: 'Food supplier not found'});
+        } else {
+            res.status(200).json(foodSupplier);
+        }
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({message: 'Internal server error'});
+    }
+}
+
+export const updateFoodSupplier = async (req: any, res: any) => {
+    try {
+        const { id } = req.params;
+        const updates = req.body;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: "Invalid food supplier ID" });
+        }
+        const existingSupplier = await FoodSupplier.findById(id);
+        if (!existingSupplier) {
+           return res.status(404).json({message: 'Food supplier not found'});
+        }
+        // Update location if only one of the latitude or longitude is provided
+        updates.location = {
+            latitude: updates.location?.latitude ?? existingSupplier.location.latitude,
+            longitude: updates.location?.longitude ?? existingSupplier.location.longitude,
+        };
+
+        // if new photos are uploaded, update the photos
+        if (req.files && req.files.length > 0) {
+           if (existingSupplier.photos.length > 0 ) {
+                await deleteImages(existingSupplier.photos);
+           }
+           updates.photos = await imageUploader(req.files as Express.Multer.File[]);
+        }
+        const updatedSupplier =
+            await FoodSupplier.findByIdAndUpdate(id, updates, {new: true, runValidators: true});
+        res.status(200).json(updatedSupplier);
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({message: 'Internal server error'});
+    }
+}
+
+export const deleteFoodSupplier = async (req: any, res: any) => {
+    try {
+        const { id } = req.params;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: "Invalid food supplier ID" });
+        }
+        const foodSupplier = await FoodSupplier.findByIdAndDelete(id);
+        if (!foodSupplier) {
+            res.status(404).json({message: 'Food supplier not found'});
+        } else {
+            await deleteImages(foodSupplier.photos);
+            res.status(200).json({message: 'Food supplier deleted successfully'});
+        }
     } catch (e) {
         console.error(e);
         res.status(500).json({message: 'Internal server error'});
--- src/routes/foodSupplierRoutes.ts
+++ src/routes/foodSupplierRoutes.ts
@@ -1,10 +1,19 @@
 import {Router} from "express";
-import {addFoodSupplier} from "../controllers/foodSupplierController";
+import {
+    addFoodSupplier, deleteFoodSupplier,
+    getAllFoodSuppliers,
+    getFoodSupplierById,
+    updateFoodSupplier
+} from "../controllers/foodSupplierController";
 import {protector} from "../middleware/authMiddleware";
 import {upload} from "../middleware/upload";
 
 const router = Router();
 
 router.post('/', protector, upload, addFoodSupplier);
+router.get('/', protector, getAllFoodSuppliers);
+router.get('/:id', protector, getFoodSupplierById);
+router.patch('/:id', protector, upload, updateFoodSupplier);
+router.delete('/:id', protector, deleteFoodSupplier);
 
 export default router;

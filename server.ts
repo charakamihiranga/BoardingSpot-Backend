f9ba413ab678eaabcbb05b0d3f6dcb49cc0544a7
charaka mihiranga <charakavmihiranga@gmail.com>
2025-03-21 14:29:06 +0530

    add resp for default endpoint

--- server.ts
+++ server.ts
@@ -6,6 +6,10 @@ const port =process.env.PORT || 3000;
 
 connectDB();
 
+app.get("/", (req, res) => {
+  res.send("Boardingspot-backend is running!");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
